66d40aedd9467c1d378f91c67548002b3e5a5ac1
Prithu Pareek <prithu.pareek2019@gmail.com>
2020-05-16 01:42:45 -0400

    Fixed login bug

--- farm-to-table/src/login.js
+++ farm-to-table/src/login.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./style.scss";
-import Login from "./components/Login.jsx";
+import Login from "./components/Login";
 
 ReactDOM.render(<Login />, document.getElementById("root"));
