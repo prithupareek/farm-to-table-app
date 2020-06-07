3fa57eea757f87ed0ca0b9d54ec69cdc0996d167
Prithu Pareek <prithu.pareek2019@gmail.com>
2020-06-07 14:31:08 -0400

    Coming soon page done

--- client/src/components/ComingSoon.jsx
+++ client/src/components/ComingSoon.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-// import "../style.scss";
+import "../style.scss";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-// Import components
-import NavigationBar from "./NavigationBar";
+import { FaInstagram, FaFacebook } from "react-icons/fa";
 
 class Login extends React.Component {
   render() {
@@ -12,19 +11,55 @@ class Login extends React.Component {
       <div className="d-flex vh-100">
         <Row className="flex-fill no-gutters">
           <Col className="d-flex flex-column h-100">
-            <Row className="flex-fill no-gutters p-3">
+            <Row className="d-flex pt-5">
               <div className="col-sm-12 my-auto text-center">
+                <h1>Farm to Table</h1>
+                <h3>Something really great is coming soon!</h3>
+                <h5>Follow us on social media to stay tuned for updates.</h5>
+              </div>
+            </Row>
+            <Row className="d-flex pt-5 justify-content-center">
+              <div className="col-sm-6">
+                <div className="embed-responsive embed-responsive-16by9">
+                  <iframe src="https://www.youtube.com/embed/ukgCbXhFWsA"></iframe>
+                </div>
+              </div>
+            </Row>
+            <Row className="d-flex pt-5">
+              <div className="col-sm-12 text-center my-auto">
                 <a
-                  href="/loginRedirect"
-                  className="btn btn-lg btn-outline-primary mx-auto"
+                  href="https://www.instagram.com/farm.to.table.app/"
+                  className="btn"
                 >
-                  &nbsp;&nbsp;&nbsp;Coming Soon&nbsp;&nbsp;&nbsp;
+                  <FaInstagram />
+                </a>
+                <a
+                  href="https://www.facebook.com/farm.to.table.app1/"
+                  className="btn"
+                >
+                  <FaFacebook></FaFacebook>
                 </a>
               </div>
             </Row>
           </Col>
         </Row>
       </div>
+
+      // <Container fluid>
+      //   <Row>
+      //     <h1>Farm to Table</h1>
+      //   </Row>
+      //   <Row>
+      // <iframe
+      //   width="auto"
+      //   height="100%"
+      //   src="https://www.youtube.com/embed/ukgCbXhFWsA"
+      //   frameborder="0"
+      //   allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      //   allowfullscreen
+      // ></iframe>
+      //   </Row>
+      // </Container>
     );
   }
 }
